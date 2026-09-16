964e1f39d5573d3870bb57378d04470485f22633
AdrianEHM24 <adrianeleuterio247518@gmail.com>
2026-09-16 17:42:23 -0600

    P12SP | Se modifico el script consultas generales AEHM

--- DATABASE/SCRIPTS/Consultasgenerales.sql
+++ DATABASE/SCRIPTS/Consultasgenerales.sql
@@ -1,3 +1,13 @@
+/*
+ P12R01_Sputify
+ Autores: Adrián Manuel Meneses Lopez, Adrián Eleuterio Hernández Martínez
+ FECHA: 15/09/2026
+*/
+
+--=============================
+-- CONSULTAS GENERALES
+--=============================
+
 USE sputiffy;
 GO
 SELECT * FROM Favoritos;
@@ -5,5 +15,20 @@ SELECT * FROM Usuarios;
 SELECT * FROM CancionesOffline;
 SELECT * FROM Canciones;
 SELECT * FROM Artistas;
-
 SELECT * FROM AudiolibrosGuardados
+
+EXEC sp_help '[dbo].[Albumes]'
+EXEC sp_help '[dbo].[Artistas]'
+EXEC sp_help '[dbo].[AudiolibrosGuardados]'
+EXEC sp_help '[dbo].[Canciones]'
+EXEC sp_help '[dbo].[CancionesOffline]'
+EXEC sp_help '[dbo].[Favoritos]'
+EXEC sp_help '[dbo].[Generos]'
+EXEC sp_help '[dbo].[HistorialReproduccion]'
+EXEC sp_help '[dbo].[PlaylistCanciones]'
+EXEC sp_help '[dbo].[PlaylistDetalle]'
+EXEC sp_help '[dbo].[Playlists]'
+EXEC sp_help '[dbo].[PreferenciasUsuario]'
+EXEC sp_help '[dbo].[RecomendacionesUsuario]'
+EXEC sp_help '[dbo].[Roles]'
+EXEC sp_help '[dbo].[Usuarios]'
